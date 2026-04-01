3969665a6e142727ff1811a8e6bd50ab84c43487
sb5068-cpu <sb5068@barnard.edu>
2026-03-31 23:20:09 -0400

    Fix theme provider type import

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,8 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+// The fixed import is right here:
+import { type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const [mounted, setMounted] = React.useState(false)
